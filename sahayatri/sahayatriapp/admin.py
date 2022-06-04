79c277df4e9e58db9444fd0b469174d05b72e831
Bhuwan Subedi <subedibhuwan2000@gmail.com>
2022-06-04 14:43:23 +0545

    company created dynamically and rendered dynamically

--- sahayatri/sahayatriapp/admin.py
+++ sahayatri/sahayatriapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from sahayatriapp.models import Company
 
 # Register your models here.
+admin.site.register(Company)
--- sahayatri/sahayatriapp/models.py
+++ sahayatri/sahayatriapp/models.py
@@ -1,3 +1,17 @@
+from distutils.command.upload import upload
+from pyexpat import model
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name=models.CharField(max_length=200,null=True)
+    address=models.CharField(max_length=250,null=True)
+    description=models.TextField(null=True)
+    email=models.EmailField(max_length=100,null=True)
+    phone=models.BigIntegerField(null=True)
+    logo=models.ImageField(upload_to="static/img/")
+    bgimage=models.ImageField(upload_to="static/img/")
+    
+    def __str__(self):
+        return self.name
