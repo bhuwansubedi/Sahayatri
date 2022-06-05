4e8be2579025c7cbe76e562a33313412d74daaa8
Bhuwan Subedi <subedibhuwan2000@gmail.com>
2022-06-05 16:21:36 +0545

    dynamic carousel

--- sahayatri/sahayatriapp/admin.py
+++ sahayatri/sahayatriapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from sahayatriapp.models import Company
+from sahayatriapp.models import Company,Slider
 
 # Register your models here.
 admin.site.register(Company)
+admin.site.register(Slider)
