3f4366488b124e6c238aa51a31e8e22eee2a9024
Bhuwan Subedi <subedibhuwan2000@gmail.com>
2022-07-14 11:47:43 +0545

    django dashboard setup and changes made in dashboard to add category types dynamically using jquery and ajax

--- sahayatri/sahayatriapp/forms.py
+++ sahayatri/sahayatriapp/forms.py
@@ -1,8 +1,10 @@
+from dataclasses import fields
 from logging import PlaceHolder
+from pyexpat import model
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Customer,Merchant,Product
+from .models import Customer,Merchant,Product,BudgetCategory,TypeCategory
 from django import forms
 
 from sahayatriapp.models import Customer, Merchant
@@ -37,3 +39,8 @@ class AddPackageForm(ModelForm):
 		    'category': forms.TextInput(attrs={'class':'form-control','placeholder':'Choose Category','style':'width: 50%; border-color: #19cdd0;'}),
 		    'description': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Description','style':'width: 50%; border-color: #19cdd0;'}),
 	    }
+
+class AddCategoryForm1(ModelForm):
+	class Meta:
+		model=BudgetCategory
+		fields=['name','startPrice','endPrice','status']
--- sahayatri/sahayatriapp/models.py
+++ sahayatri/sahayatriapp/models.py
@@ -1,9 +1,14 @@
 from dataclasses import asdict
 from distutils.command.upload import upload
 from email.mime import image
+from itertools import product
 from operator import mod
 from pyexpat import model
 from re import T
+from statistics import mode
+from telnetlib import STATUS
+from tkinter import CASCADE
+from tkinter.tix import Tree
 from unicodedata import category, name
 from django.db import models
 from django.contrib.auth.models import User
@@ -43,6 +48,7 @@ class Product(models.Model):
     description = models.CharField(max_length=5000,null=True,blank=True)
     posted_by = models.CharField(max_length=200,null=True)
     date_created = models.DateTimeField(auto_now_add=True,null=True)
+    status=models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -69,6 +75,7 @@ class Merchant(models.Model):
     pan_no = models.CharField(max_length=200,null=True)
     company_name = models.CharField(max_length=200,null=True)
     company_website = models.CharField(max_length=200,null=True)
+    status=models.BooleanField(default=False)
 
 
     def __str__(self):
@@ -94,6 +101,7 @@ class Customer(models.Model):
     municipality = models.CharField(max_length=200,null=True)
     age = models.CharField(max_length=200,null=True)
     gender = models.CharField(max_length=200,null=True,choices=GENDER)
+    status=models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -103,3 +111,38 @@ class Slider(models.Model):
     alttext2=models.CharField(max_length=100,null=True)
     alttext3=models.CharField(max_length=100,null=True)
     sliderimg=models.ImageField(upload_to="static/img/")
+    status=models.BooleanField(default=True)
+
+class Overview(models.Model):
+    product=models.ForeignKey(Product,related_name='overview',on_delete=models.CASCADE,null=True)
+    author=models.ForeignKey(Merchant,on_delete=models.CASCADE,null=True)
+    facts=models.TextField(null=True)
+    travelinfo=models.TextField(null=True)
+    status=models.BooleanField(default=True)
+
+class Itnerary(models.Model):
+    product=models.ForeignKey(Product,related_name='itner',on_delete=models.CASCADE,null=True)
+    author=models.ForeignKey(Merchant,on_delete=models.CASCADE,null=True)
+    day=models.CharField(max_length=250,default='Day 1',null=True)
+    detail=models.TextField(null=True)
+    status=models.BooleanField(default=True)
+
+class BudgetCategory(models.Model):
+    name=models.CharField(max_length=250,null=True)
+    startPrice=models.BigIntegerField(null=True)
+    endPrice=models.BigIntegerField(null=True)
+    status=models.BooleanField(default=True,null=True)
+    
+class TypeCategory(models.Model):
+    name=models.CharField(max_length=250,null=True)
+    status=models.BooleanField(default=True,null=True)
+
+class ImageCollection(models.Model):
+    product=models.ForeignKey(Product,related_name='img',on_delete=models.CASCADE,null=True)
+    author=models.ForeignKey(Merchant,on_delete=models.CASCADE,null=True)
+    image1=models.ImageField(upload_to="static/img/")
+    image2=models.ImageField(upload_to="static/img/")
+    image3=models.ImageField(upload_to="static/img/")
+    image4=models.ImageField(upload_to="static/img/")
+    
+
