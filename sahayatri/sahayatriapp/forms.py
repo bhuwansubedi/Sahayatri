254f546bcf223d0a7b84064fb1abe944c63b5776
Bhuwan Subedi <subedibhuwan2000@gmail.com>
2022-09-22 09:14:12 +0545

    final merge for internal defense

--- sahayatri/sahayatriapp/forms.py
+++ sahayatri/sahayatriapp/forms.py
@@ -4,10 +4,8 @@ from pyexpat import model
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Customer,Merchant,Product,BudgetCategory,TypeCategory
 from django import forms
 
-from sahayatriapp.models import Customer, Merchant
 
 
 class CreateUserForm(UserCreationForm):
@@ -20,27 +18,3 @@ class CreateMerchantForm(UserCreationForm):
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 
-class AddPackageForm(ModelForm):
-    class Meta:
-        model = Product
-        fields = ['name','price','image','category','description']
-        labels = {
-			'name': '',
-		    'price': '',
-		    'image': '',
-		    'category': '',
-		    'description': '',
-
-		}
-        widgets = {
-	    	'name': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Package Name..','style':'width: 50%; border-color: #19cdd0;'}),
-		    'price': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Package Price','style':'width: 50%; border-color: #19cdd0;'}),
-		    'image': forms.FileInput(attrs={'class':'form-control','placeholder':'Upload Image','style':'width: 50%; border-color: #19cdd0;'}),
-		    'category': forms.TextInput(attrs={'class':'form-control','placeholder':'Choose Category','style':'width: 50%; border-color: #19cdd0;'}),
-		    'description': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Description','style':'width: 50%; border-color: #19cdd0;'}),
-	    }
-
-class AddCategoryForm1(ModelForm):
-	class Meta:
-		model=BudgetCategory
-		fields=['name','startPrice','endPrice','status']
